fb1da4298f04f625b9d465a1793295f4032288f4
Michael J. Schmitt <michael@michaelschmitt.me>
2018-01-21 18:52:06 -0500

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's2_@fie)ew^0b-musr-o8qkt*knve^xsbro$10gug9lwgu+hff'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'xschmitty.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
